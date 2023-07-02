329103335e423e06043efd7102959df38748f972
docker3 <MY_NAME@example.com>
2023-07-02 09:56:16 +0200

    Upload and download speed exporter

--- src/main.go
+++ src/main.go
@@ -24,6 +24,12 @@ func speedTest() {
 
 		prometheus.MustRegister(speedTestResultLatency)
 		speedTestResultLatency.Set(float64(s.Latency.Microseconds()) / 1000)
+
+		prometheus.MustRegister(speedTestResultDLSpeed)
+		speedTestResultDLSpeed.Set(s.DLSpeed)
+
+		prometheus.MustRegister(speedTestResultULSpeed)
+		speedTestResultULSpeed.Set(s.ULSpeed)
 	}
 
 }
@@ -34,6 +40,14 @@ var (
 		Name: "speedtest_latency",
 		Help: "Latency of the speedtest in ms",
 	})
+	speedTestResultDLSpeed = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "speedtest_download_speed",
+		Help: "Download speed of the speedtest in Mbps",
+	})
+	speedTestResultULSpeed = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "speedtest_upnload_speed",
+		Help: "Upnload speed of the speedtest in Mbps",
+	})
 )
 
 func main() {
@@ -41,6 +55,6 @@ func main() {
 	go speedTest()
 
 	http.Handle("/metrics", promhttp.Handler())
-	http.ListenAndServe(":8080", nil)
-	fmt.Printf("Serving /metrics at :8080")
+	http.ListenAndServe(":2112", nil)
+	fmt.Printf("Serving /metrics at :2112")
 }
